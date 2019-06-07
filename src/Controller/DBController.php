ac93018b65306edd329af433b6ea868dbf8ffbf6
MaikK <m.krisanov@yahoo.de>
2019-06-07 11:35:02 +0200

    Added Create Password Validation Backend Endpoint: /user/login/password-validation

--- src/Controller/DBController.php
+++ src/Controller/DBController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,13 +16,38 @@ class DBController extends AbstractController
     {
         $entityManager = $this->getDoctrine()->getManager();
         $data = $_POST;
-        // TODO - validierung und so
+        //TODO - validierung
         $newUser = new User();
         $newUser->setUsername($data["username"]);
         $newUser->setPassword(password_hash($data["password"], PASSWORD_DEFAULT));
         $newUser->setEmail($data["email"]);
         $entityManager->persist($newUser);
         $entityManager->flush();
-        return new Response("New user created!", Response::HTTP_OK);
+        return new Response("New user created!", Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("/user/login/password-validation", methods="GET")
+     */
+    public function passwordValidation($data)
+    {
+        if(is_null($data["username"]) || is_null($data["password"]))
+        {
+            return new Response("Missing Username or Password", Response::HTTP_BAD_REQUEST);
+        } else {
+            $username = $data["username"];
+            $password = $data["password"];
+            $entityManager = $this->getDoctrine()->getRepository(User::class);
+            $checkUser = $entityManager->findOneBy(['username' => $username]);
+            if(!$checkUser)
+                return new Response("User " . $username . " does not exist.");
+            else
+            {
+                if($checkUser->getPassword() == $password)
+                    return new Response("Password Correct", Response::HTTP_ACCEPTED);
+                else
+                    return new Response("Username or Password wrong", Response::HTTP_NOT_ACCEPTABLE);
+            }
+        }
     }
 }

2651a16239e5d16c68b610d08603a8a5325e8a38
MaikK <m.krisanov@yahoo.de>
2019-06-07 12:08:37 +0200

    Change Password Verify Backend Endpoint: /user/login/password-validation

--- src/Controller/DBController.php
+++ src/Controller/DBController.php
@@ -27,26 +27,29 @@ class DBController extends AbstractController
     }
 
     /**
-     * @Route("/user/login/password-validation", methods="GET")
+     * @Route("/user/login/password-validation", methods="POST")
      */
-    public function passwordValidation($data)
+    public function passwordValidation()
     {
-        if(is_null($data["username"]) || is_null($data["password"]))
+        if(is_null($_POST["username"]) || is_null($_POST["password"]))
         {
-            return new Response("Missing Username or Password", Response::HTTP_BAD_REQUEST);
+            return new Response('{}', Response::HTTP_BAD_REQUEST,
+                ['content-type' => 'json']);
         } else {
-            $username = $data["username"];
-            $password = $data["password"];
+            $username = $_POST["username"];
+            $password = $_POST["password"];
             $entityManager = $this->getDoctrine()->getRepository(User::class);
             $checkUser = $entityManager->findOneBy(['username' => $username]);
             if(!$checkUser)
-                return new Response("User " . $username . " does not exist.");
+                return new Response('{}', Response::HTTP_NOT_FOUND,
+                    ['content-type'=>'json']);
             else
             {
                 if(password_verify($password, $checkUser->getPassword()))
-                    return new Response("Password Correct", Response::HTTP_ACCEPTED);
+                    return new Response($checkUser->getId(), Response::HTTP_OK);
                 else
-                    return new Response("Username or Password wrong", Response::HTTP_NOT_ACCEPTABLE);
+                    return new Response('{}', Response::HTTP_FORBIDDEN,
+                        ['content-type'=>'json']);
             }
         }
     }
